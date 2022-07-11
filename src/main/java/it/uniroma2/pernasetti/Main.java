f220149a02974bc43a9192a69f1399e91922c4b4
Jace <GitHub 1>
2022-07-11 11:14:01 +0200

    Final Commit

--- src/main/java/it/uniroma2/pernasetti/Main.java
+++ src/main/java/it/uniroma2/pernasetti/Main.java
@@ -1,3 +1,4 @@
+package it.uniroma2.pernasetti;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -6,18 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> myList = new ArrayList<String>();
+        List<String> myList = new ArrayList<>();
         if (args.length > 1) {
             for (int i = 0; i < args.length; i++) {
-                myList.add(args[i]);
+                Collections.addAll(myList, args[i]);
             }
 
-            Collections.sort(myList);
-            Collections.reverse(myList);
+            Collections.sort(myList, Collections.reverseOrder());
 
             System.out.println(myList);
-        }else{
-            System.out.println("Parametri macanti");
         }
     }
 }
